f0bf12278e854f447e16429ea710ce390f19825b
Richard Kendall <richard.kendall@gmail.com>
2020-08-22 20:57:24 +1000

    changes to get scan of index working with like

--- add_location_table_indexes.sql
+++ add_location_table_indexes.sql
@@ -4,12 +4,13 @@ create index levelnumber_idx on location_table(levelnumber);
 create index lotnumber_idx on location_table(lotnumber);
 create index roadnumber1_idx on location_table(roadnumber1);
 create index roadnumber2_idx on location_table(roadnumber2);
-create index roadname_idx on location_table(roadname);
-create index localityname_idx on location_table(localityname);
+create index roadname_idx on location_table(roadname varchar_pattern_ops);
+create index localityname_idx on location_table(localityname varchar_pattern_ops);
 create index postcode_idx on location_table(postcode);
 create index state_idx on location_table(statecode);
 create index lat_idx on location_table(latitude);
 create index long_idx on location_table(longitude);
+create index addresssitename_idx on location_table(addresssitename varchar_pattern_ops);
 
 
 
--- create_view.sql
+++ create_view.sql
@@ -1,3 +1,5 @@
+drop view if exists LOCATION_VIEW;
+
 create or replace view LOCATION_VIEW
 
 as
@@ -14,7 +16,7 @@ concat(ad.lot_number, ad.lot_number_suffix) as lotnumber,
 concat(ad.number_first, ad.number_first_suffix) as roadnumber1,
 concat(ad.number_last, ad.number_last_suffix) as roadnumber2,
 sl.street_name as roadname,
-sl.street_type_code as roadtypecode,
+sta.name as roadtypecode,
 l.locality_name as localityname,
 ad.postcode as postcode,
 st.state_abbreviation as statecode,
@@ -26,6 +28,7 @@ from
 ADDRESS_DETAIL ad
 left join LOCALITY l on l.locality_pid = ad.locality_pid 
 left join STREET_LOCALITY sl on sl.street_locality_pid = ad.street_locality_pid 
+left join STREET_TYPE_AUT sta on sta.code = sl.street_type_code 
 left join STATE st on st.state_pid = l.state_pid 
 left join ADDRESS_DEFAULT_GEOCODE adg on adg.address_detail_pid = ad.address_detail_pid
 
